f998a2fa9f2f86ddf444cf38097f3f4a03f6854b
Yu-Jou Lai <uro004300@icloud.com>
2026-03-26 14:57:29 +0800

    DEV-11 implement TwoSumArray

---
+++ src/twosum.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+using namespace std;
+
+vector<int> TwoSumArray(vector<int>& nums, int target) {
+    for (int i = 0; i < nums.size(); i++) {
+        for (int j = i + 1; j < nums.size(); j++) {
+            if (nums[i] + nums[j] == target) {
+                return {i, j};
+            }
+        }
+    }
+    return {};
+}
